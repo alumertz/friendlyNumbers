f5047527333cb25ebd9816d26514817be09343d1
beer <yasminbeerz@gmail.com>
2024-11-21 18:49:21 -0300

    added omp parallel and time measurement

--- main.c
+++ main.c
@@ -22,42 +22,57 @@ void friendly_numbers(long int start, long int end) {
 
     long int i, j, factor, ii, sum, done, n;
 
-    //calcula soma dos divisores para cada numero do intervalo [start,end]
-    #pragma omp parallel for private(factor, done, sum, ii) schedule(dynamic)
-    for (i = start; i <= end; i++) {
-        ii = i - start; // indice ajustado pra comecar de 0
-        sum = 1 + i; //inicializa soma dos div com 1
-        the_num[ii] = i; //numero usado
-        done = i; // limite para factor, é atualizado dps
-        factor = 2; // fator de divisao
+        //calcula soma dos divisores para cada numero do intervalo [start,end]
+//    #pragma omp parallel shared(num, den, the_num, n) reduction(+:sum)
+//    {
+//        #pragma omp for
+        for (i = start; i <= end; i++) {
+            ii = i - start; // indice ajustado pra comecar de 0
+            sum = 1 + i; //inicializa soma dos div com 1
+            the_num[ii] = i; //numero usado
+            done = i; // limite para factor, é atualizado dps
+            factor = 2; // fator de divisao
 
-        //testa fatores e soma eles
-        #pragma omp simd reduction(+:sum)
-        while (factor < done){
-            if ((i % factor) == 0){ //poderia ser compartillhado
-                sum += (factor + (i / factor));
-                if ((done = i / factor) == factor)
-                    sum -= factor;
+            //testa fatores e soma eles
+            while (factor < done){
+                if ((i % factor) == 0){ //poderia ser compartilhado
+                    sum += (factor + (i / factor));
+                    if ((done = i / factor) == factor)
+                        sum -= factor;
+                }
+                factor++;
             }
-            factor++;
-        }
-        num[ii] = sum;
-        den[ii] = i;
 
-        //calcula fracao simplificada
-        n = gcd(num[ii], den[ii]);
-        num[ii] /= n;
-        den[ii] /= n;
-    }
+            num[ii] = sum;
+            den[ii] = i;
 
-    #pragma omp parallel for private(j) schedule(dynamic) nowait
-    for (i = 0; i < last; i++) {
-        for (j = i + 1; j < last; j++) {
-            if ((num[i] == num[j]) && (den[i] == den[j]))
-                printf("%ld and %ld are FRIENDLY\n",
-                the_num[i], the_num[j]);
+            //calcula fracao simplificada
+                n = gcd(num[ii], den[ii]);
+                num[ii] /= n;
+                den[ii] /= n;
+
         }
-    }
+
+//    }
+
+    // abaixo funciona
+    // outra sugestão: pragma omp for collapse(2) private (i, j).
+    // outra sugestão: pragma omp loop
+    // 1 threads: 0.001424, 0.001050, 0.001084
+    // 2 threads: 0.001070, 0.000908, 0.000983
+    // 4 threads: 0.000700, 0.000571, 0.000748
+    // 8 threads: 0.000884, 0.000676, 0.000574
+       #pragma omp parallel private(i, j) shared(den, num, the_num) num_threads(12)
+        {
+            #pragma omp for
+            for (i = 0; i < last; i++) {
+                for (j = i + 1; j < last; j++) {
+                    if ((num[i] == num[j]) && (den[i] == den[j]))
+                        printf("%ld and %ld are FRIENDLY\n",
+                        the_num[i], the_num[j]);
+                }
+            }
+        }
 
     free(the_num);
     free(num);
@@ -67,19 +82,19 @@ void friendly_numbers(long int start, long int end) {
 int main(int argc, char **argv) {
     long int start;
     long int end;
+    double timer_start, timer_end;
 
-    #pragma omp parallel {
-        #pragma omp single {
-            while(1){
-                scanf("%ld%ld", &start, &end);
-                if (start == 0 && end == 0)
-                    break;
-                printf("Number %ld to %ld\n", start, end);
-                #pragma omp task firstprivate(start, end){
-                    friendly_numbers(start, end);
-                }
-            }
-        }
+    while(1){
+        scanf("%ld%ld", &start, &end);
+        if (start == 0 && end == 0)
+            break;
+        printf("Number %ld to %ld\n", start, end);
+
+        timer_start = omp_get_wtime();
+        friendly_numbers(start, end);
+        timer_end = omp_get_wtime();
+        printf("Work took %f seconds\n\n", timer_end - timer_start);
     }
+
     return 0;
 }
